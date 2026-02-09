cd86e90cec9683d8502d293770ab94841e48219b
Utsho Kumar Dey <utshokumerdey@gmail.com>
2026-02-09 23:49:39 +0600

    Update calculate.php

--- CSE_Fest_Project/calculate.php
+++ CSE_Fest_Project/calculate.php
@@ -13,7 +13,7 @@
 // 5. Constructs and executes a shell command to run the Python script (`calculate.py`).
 // 6. Captures and decodes the JSON output from the Python script.
 // 7. Inserts the inputs and results into the MySQL database..
-// 8. Stores the results in the $_SESSION superglobal..
+// 8. Stores the results in the $_SESSION superglobal...
 // 9. Redirects the user to `result.php` to display the results..
 
  // Start a session to pass the calculation results to the result page.
